a252687b5322c5eacbe0f02b9b79d919705ef18b
Fernandez, Tony (Extern) <Tony.Fernandez.extern@pyur.com>
2025-03-06 09:09:44 +0100

    Initial commit

---
+++ src/main/java/com/pyur/fiber/optic/network/FiberOpticNetworkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pyur.fiber.optic.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiberOpticNetworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiberOpticNetworkServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyur/fiber/optic/network/FiberOpticNetworkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyur.fiber.optic.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiberOpticNetworkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
